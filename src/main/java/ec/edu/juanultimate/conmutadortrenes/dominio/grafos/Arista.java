27a6d767ed56a54036b56e37e8ec0f08a8b0fb0a
Juan Gabriel Guzmán <juanultimate@hotmail.com>
2015-10-12 22:20:07 -0500

    Versión inicial

--- src/main/java/ec/edu/juanultimate/conmutadortrenes/dominio/grafos/Arista.java
+++
@@ -1,5 +0,0 @@
-package ec.edu.juanultimate.conmutadortrenes.dominio.grafos;
-public interface Arista<V extends Vertice> {
-    V getVerticeInicio();
-    V getVerticeFin();
-}
--- src/main/java/ec/edu/juanultimate/conmutadortrenes/expcetion/CaminoNoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.juanultimate.conmutadortrenes.expcetion;
-
-public class CaminoNoEncontradoException extends RuntimeException {
-
-
-    public CaminoNoEncontradoException(final String verticeInicio, final String verticeFin) {
-        super("No route exists between " + verticeInicio + " and " + verticeFin);
-    }
-}
